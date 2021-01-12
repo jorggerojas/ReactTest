33a5eebea7af2084d3e7c6a9631722bc682e9587
Jorge Luis Rojas Arcos <jorgelroar@gmail.com>
2021-01-12 15:07:51 -0600

    feat: add dependencies and start server

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+const App = () => {
+  console.log(1 + 1)
+  return (
+    <div>
+      <h1> Hello, World! </h1>
+    </div>
+  );
+};
+export default App;
